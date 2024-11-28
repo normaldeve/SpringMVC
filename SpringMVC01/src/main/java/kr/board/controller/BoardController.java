98e5bd0c5aae831534f527ea958b91864442aca3
junwoo <junnukim1007@gmail.com>
2024-11-28 17:03:03 +0900

    mvc01

--- SpringMVC01/src/main/java/kr/board/controller/BoardController.java
+++ SpringMVC01/src/main/java/kr/board/controller/BoardController.java
@@ -1,33 +1,35 @@
 package kr.board.controller;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import kr.board.entity.Board;
 
 @Controller
-public class BoardController {
+public class BoardController{
 	// HandlerMapping
 	@RequestMapping("/boardList.do")
 	public String boardList(Model model) {
-		
-		Board bo = new Board();
-		bo.setIdx(1);
-		bo.setTitle("게시판 작성");
-		bo.setContents("게시판 연습을 해보자");
-		bo.setWriter("김준우");
-		bo.setIndate("2024-11-28");
-		bo.setCount(0);
+		Board vo = new Board();
+		vo.setIdx(1);
+		vo.setTitle("게시판연습");
+		vo.setContent("content");
+		vo.setWriter("my");
+		vo.setIndate("2024-11-12");
+		vo.setCount(1);
 		
 		List<Board> list = new ArrayList<Board>();
-		list.add(bo);
-		list.add(bo);
-		list.add(bo);
-		model.addAttribute("list", list); // 객체 바인딩 가장 중요한 작업
-		
-		return "boardList"; //WEB_INF/views/
-	}
+		list.add(vo);
+		model.addAttribute("list", list);
+		return "boardList"; // /WEB-INF/views/boardList.jsp -> forward
+	}	
 }
--- SpringMVC01/src/main/java/kr/board/entity/Board.java
+++ SpringMVC01/src/main/java/kr/board/entity/Board.java
@@ -2,12 +2,13 @@ package kr.board.entity;
 
 import lombok.Data;
 
-@Data
+@Data //- Lombok API
 public class Board {
-	private int idx;
-	private String title;
-	private String contents;
-	private String writer;
-	private String indate; // 작성일
-	private int count; // 조회
+  private int idx; // 번호
+  private String title; // 제목
+  private String content; // 내용
+  private String writer; // 작성자
+  private String indate; // 작성일
+  private int count; // 조회수
+  // setter , getter 
 }
